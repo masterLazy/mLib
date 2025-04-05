4f3b6c5d2d8e60be8b458fd2152f856613f7dfc7
mLazy <msterlazy@qq.com>
2025-04-06 00:00:46 +0800

    Update; Add result.hpp & function.hpp

--- console/console.hpp
+++ console/console.hpp
@@ -4,6 +4,93 @@
 * Console: 用于控制台的实用工具
 */
 
-#include <iostream>
+#include <cstdio>
+#include <string>
+#include <sstream>
+// 对 Windows.h 要做好充足防御。
+namespace win {
+#include <Windows.h>
+}
+#undef min
+#undef max
 
-#include "progress.hpp"		// 进度条
+namespace mlib {
+	namespace console {
+		class Console {
+			win::HANDLE hStdOut;
+			win::CONSOLE_SCREEN_BUFFER_INFO bufferInfo;
+			bool fetch_bufferinfo() {
+				return win::GetConsoleScreenBufferInfo(hStdOut, &bufferInfo);
+			}
+		public:
+			Console(win::HANDLE hStdOut = NULL) {
+				init(hStdOut);
+			}
+
+			bool init(win::HANDLE hStdOut = NULL) {
+				using namespace win;
+				if (!hStdOut) {
+					hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+					if (!hStdOut) {
+						return false;
+					}
+				}
+				this->hStdOut = hStdOut;
+				return win::GetConsoleScreenBufferInfo(hStdOut, &bufferInfo);
+			}
+
+			short get_width() const {
+				return bufferInfo.dwSize.X;
+			}
+			short get_height() const {
+				return bufferInfo.dwSize.Y;
+			}
+			win::COORD get_screen_size() const {
+				return bufferInfo.dwSize;
+			}
+			win::HANDLE get_stdout_handle() const {
+				return hStdOut;
+			}
+			win::CONSOLE_SCREEN_BUFFER_INFO get_screen_buffer_info() const {
+				return bufferInfo;
+			}
+
+			bool set_width(short width) {
+				bufferInfo.dwSize.X = width;
+				return SetConsoleScreenBufferSize(hStdOut, bufferInfo.dwSize);
+			}
+			bool set_height(short height) {
+				bufferInfo.dwSize.Y = height;
+				return SetConsoleScreenBufferSize(hStdOut, bufferInfo.dwSize);
+			}
+			bool set_screen_size(const win::COORD size) {
+				bufferInfo.dwSize = size;
+				return SetConsoleScreenBufferSize(hStdOut, bufferInfo.dwSize);
+			}
+			bool set_screen_size(short width, short height) {
+				return set_screen_size(win::COORD({ width, height }));
+			}
+
+			bool clear_screen() {
+				using namespace win;
+				/* https://learn.microsoft.com/zh-cn/windows/console/clearing-the-screen */
+				COORD coordScreen = { 0, 0 };
+				DWORD cCharsWritten;
+				DWORD dwConSize;
+				dwConSize = get_width() * get_height();
+				if (!FillConsoleOutputCharacter(hStdOut, (TCHAR)' ', dwConSize,
+					coordScreen, &cCharsWritten)) {
+					return false;
+				}
+				if (!FillConsoleOutputAttribute(hStdOut, bufferInfo.wAttributes, dwConSize,
+					coordScreen, &cCharsWritten)) {
+					return false;
+				}
+				SetConsoleCursorPosition(hStdOut, coordScreen);
+				return true;
+			}
+		};
+	} // namespace console
+} // namespace mlib
+
+#include "progressbar.hpp"		// 进度条
--- fastmath/amp/amp_device.hpp
+++ fastmath/amp/amp_device.hpp
@@ -8,40 +8,42 @@
 */
 #include "../fastmath.hpp"
 namespace mlib {
-	namespace amp {
-		class Device : public fastmath::Device<concurrency::accelerator> {
-		public:
-			Device() {}
-			Device(Concurrency::accelerator _device) {
-				device = _device;
-			}
+	namespace fastmath {
+		namespace amp {
+			class Device : public fastmath::Device<concurrency::accelerator> {
+			public:
+				Device() {}
+				Device(Concurrency::accelerator _device) {
+					device = _device;
+				}
 
-			std::wstring getInfo() const override {
-				return device.description;
+				std::wstring getInfo() const override {
+					return device.description;
+				}
+				virtual size_t getMemKB() const override {
+					return device.dedicated_memory;
+				}
+				bool isFp32Available() const override {
+					return device.supports_double_precision;
+				}
+				bool isGpu() const override {
+					return !device.is_emulated;
+				}
+			};
+			// 获取所有设备
+			std::vector<Device> getDevices() {
+				using namespace concurrency;
+				std::vector<Device> devices;
+				std::vector<accelerator> accs = accelerator::get_all();
+				for (auto i : accs) {
+					devices.push_back(Device(i));
+				}
+				return devices;
 			}
-			virtual size_t getMemKB() const override {
-				return device.dedicated_memory;
+			// 获取默认设备
+			Device getDefaultDevice() {
+				return getDevices()[0];
 			}
-			bool isFp32Available() const override {
-				return device.supports_double_precision;
-			}
-			bool isGpu() const override {
-				return !device.is_emulated;
-			}
-		};
-		// 获取所有设备
-		std::vector<Device> getDevices() {
-			using namespace concurrency;
-			std::vector<Device> devices;
-			std::vector<accelerator> accs = accelerator::get_all();
-			for (auto i : accs) {
-				devices.push_back(Device(i));
-			}
-			return devices;
-		}
-		// 获取默认设备
-		Device getDefaultDevice() {
-			return getDevices()[0];
-		}
-	} // namespace amp
+		} // namespace amp
+	} // namespace fastmath
 } // namespace mlib
