4b66df695751c95550f40ad7a73b2fc435812dfa
mLazy <msterlazy@qq.com>
2025-09-05 22:26:48 +0800

    Add mlib::Process::readLine()

--- include/mlib/Process.hpp
+++ include/mlib/Process.hpp
@@ -1,6 +1,6 @@
 #pragma once
 /**
- * @file		process.hpp
+ * @file		Process.hpp
  * @brief		包装一个进程，可以对其进行 Std I/O 操作
  *
  * @author		masterLazy
@@ -11,141 +11,154 @@
 #undef min
 #undef max
 #include <string>
-#include <exception>
+#include <stdexcept>
+#include <thread>
+#include <mutex>
+#include <deque>
 
 namespace mlib {
-	namespace process {
+	/**
+	 * @brief	包装一个进程，可以对其进行 Std I/O 操作
+	 */
+	class Process {
+		HANDLE to_hRead = NULL, to_hWrite = NULL;
+		HANDLE from_hRead = NULL, from_hWrite = NULL;
+		PROCESS_INFORMATION process_info;
+		// 读取线程
+		std::thread* readThread;
+		int signal;
+		const int SG_NULL = 0;
+		const int SG_EXIT = 1;
+		std::mutex mtx;
+		std::deque<char> readBuffer;
+		void readThreadHandler() {
+			DWORD bytes_read;
+			char buf;
+			while (signal != SG_EXIT) {
+				if (ReadFile(from_hRead, &buf, 1, &bytes_read, NULL) && bytes_read) {
+					std::unique_lock<std::mutex> lk(mtx);
+					readBuffer.push_back(buf);
+				}
+			}
+		}
+	public:
 		/**
-		 * @brief	包装一个进程，可以对其进行 Std I/O 操作
-		 */
-		class Process {
-		private:
-			HANDLE to_hRead = NULL, to_hWrite = NULL;
-			HANDLE from_hRead = NULL, from_hWrite = NULL;
-			PROCESS_INFORMATION process_info;
-		public:
-			/**
-			* @brief				实例化进程
-			* @param command_line	要执行的命令行
-			* @param show_window	是否要显示窗口
-			* @exception std::exception 此函数会抛出异常！
-			*/
-			Process(const std::string& command_line, bool show_window = false) {
-				// 创建管道
-				SECURITY_ATTRIBUTES se = { 0 };
-				se.lpSecurityDescriptor = NULL;
-				se.bInheritHandle = TRUE;
-				se.nLength = sizeof(se);
-				if (CreatePipe(&to_hRead, &to_hWrite, &se, 0) == 0) {
-					throw std::exception("Failed to create pipe");
-				}
-				if (CreatePipe(&from_hRead, &from_hWrite, &se, 0) == 0) {
-					throw std::exception("Failed to create pipe");
-				}
-				// 创建进程
-				STARTUPINFOA sInfo = { 0 };
-				sInfo.dwFlags = STARTF_USESHOWWINDOW | STARTF_USESTDHANDLES;
-				sInfo.wShowWindow = show_window ? SW_SHOW : SW_HIDE;
-				sInfo.hStdInput = to_hRead;
-				sInfo.hStdOutput = from_hWrite;
-				sInfo.hStdError = from_hWrite;
-				if (CreateProcessA(NULL, (char*)command_line.c_str(), NULL, NULL, TRUE, NULL, NULL, NULL, &sInfo, &process_info) == 0) {
-					throw std::exception("Failed to create process");
-				}
+		* @brief				实例化进程
+		* @param command_line	要执行的命令行
+		* @param show_window	是否要显示窗口
+		* @exception std::runtime_error 此函数会抛出异常！
+		*/
+		Process(const std::string& command_line, bool show_window = false) {
+			// 创建管道
+			SECURITY_ATTRIBUTES se = { 0 };
+			se.lpSecurityDescriptor = NULL;
+			se.bInheritHandle = TRUE;
+			se.nLength = sizeof(se);
+			if (CreatePipe(&to_hRead, &to_hWrite, &se, 0) == 0) {
+				throw std::runtime_error("Failed to create pipe");
 			}
-			~Process() {
-				if (to_hWrite) {
-					CloseHandle(to_hWrite);
-				}
-				if (from_hRead) {
-					CloseHandle(from_hRead);
-				}
-				CloseHandle(process_info.hProcess);
+			if (CreatePipe(&from_hRead, &from_hWrite, &se, 0) == 0) {
+				throw std::runtime_error("Failed to create pipe");
 			}
-			/**
-			* @brief	向进程的 stdin 写入数据
-			* @return	实际写入的字节数
-			* @exception std::exception 写入失败时抛出异常
-			*/
-			DWORD write(const std::string& data) {
-				DWORD bytes_written;
-				if (WriteFile(to_hWrite, data.c_str(), data.size(), &bytes_written, NULL) == 0) {
-					throw std::exception("Failed to write to pipe");
-				}
-				return bytes_written;
+			// 创建进程
+			STARTUPINFOA sInfo = { 0 };
+			sInfo.dwFlags = STARTF_USESHOWWINDOW | STARTF_USESTDHANDLES;
+			sInfo.wShowWindow = show_window ? SW_SHOW : SW_HIDE;
+			sInfo.hStdInput = to_hRead;
+			sInfo.hStdOutput = from_hWrite;
+			sInfo.hStdError = from_hWrite;
+			if (CreateProcessA(NULL, (char*)command_line.c_str(), NULL, NULL, TRUE, NULL, NULL, NULL, &sInfo, &process_info) == 0) {
+				throw std::runtime_error("Failed to create process");
 			}
-			/**
-			* @brief	返回 stdout 中的字节数(不删除)
-			* @return	stdout 中的字节数
-			* @exception std::exception peek 失败时抛出异常
-			*/
-			DWORD peek() {
-				DWORD bytes;
-				if (PeekNamedPipe(from_hRead, NULL, NULL, NULL, &bytes, NULL) == 0) {
-					throw std::exception("Failed to peek pipe");
-				}
-				return bytes;
+			// 创建读取线程
+			readThread = new std::thread(&Process::readThreadHandler, this);
+			signal = NULL;
+			readThread->detach();
+		}
+		~Process() {
+			// 通知读取线程退出
+			clock_t timer = clock();
+			signal = SG_EXIT;
+			while (signal == SG_EXIT && clock() - timer < 100);
+			if (signal != SG_NULL) {
+				TerminateThread(readThread->native_handle(), 0);
 			}
-			/**
-			* @brief				从进程的 stdout 读取数据
-			* @param bytes_to_read	要读取的最大字节数
-			* @return				实际读取的字节数
-			* @exception std::exception 读取失败时抛出异常
-			*/
-			std::string read(DWORD bytes_to_read = 512) {
-				DWORD bytes_read;
-				unsigned char* buf = new unsigned char[bytes_to_read];
-				if (ReadFile(from_hRead, buf, bytes_to_read, &bytes_read, NULL) == 0) {
-					delete[] buf;
-					throw std::exception("Failed to read from pipe");
-				}
-				std::string res;
-				res.resize(bytes_read);
-				for (DWORD i = 0; i < bytes_read; i++) {
-					res[i] = buf[i];
-				}
-				delete[] buf;
-				return res;
+			// 关闭句柄
+			if (to_hWrite) {
+				CloseHandle(to_hWrite);
 			}
-			/**
-			* @brief				从进程的 stdout 读取数据直到行尾
-			* @param max_bytes		要读取的最大字节数
-			* @return				实际读取的字节数
-			* @exception std::exception 读取失败时抛出异常
-			*/
-			std::string readline(DWORD max_bytes = 1024) {
-				DWORD bytes_read;
-				std::string res;
-				char buf;
-				for (DWORD i = 0; i < max_bytes; i++) {
-					if (ReadFile(from_hRead, &buf, 1, &bytes_read, NULL) == 0) {
-						throw std::exception("Failed to read from pipe");
-					}
-					if (buf == '\r' || buf == '\n') {
-						if (res.empty()) {
-							continue;
-						}
-						else return res;
-					}
-					res += buf;
-				}
-				return res;
+			if (from_hRead) {
+				CloseHandle(from_hRead);
 			}
-			/**
-			* @brief				获取进程的返回值
-			* @retval STILL_ACTIVE	进程未退出
-			*/
-			DWORD get_exit_code() {
-				DWORD code;
-				GetExitCodeProcess(process_info.hProcess, &code);
-				return code;
+			CloseHandle(process_info.hProcess);
+		}
+		/**
+		* @brief	向进程的 stdin 写入数据
+		* @return	实际写入的字节数
+		* @exception std::runtime_error 写入失败时抛出异常
+		*/
+		DWORD write(const std::string& data) {
+			DWORD bytes_written;
+			if (WriteFile(to_hWrite, data.c_str(), data.size(), &bytes_written, NULL) == 0) {
+				throw std::runtime_error("Failed to write to pipe");
 			}
-			/**
-			* @brief	强制终止进程
-			*/
-			void terminate(UINT exit_code = 0) {
-				TerminateProcess(process_info.hProcess, exit_code);
+			return bytes_written;
+		}
+		/**
+		* @brief	返回 stdout 中的字节数(不删除)
+		* @return	stdout 中的字节数
+		* @exception std::runtime_error peek 失败时抛出异常
+		*/
+		DWORD peek() {
+			return readBuffer.size();
+		}
+		/**
+		* @brief				从进程的 stdout 读取数据
+		* @param bytes_to_read	要读取的最大字节数
+		* @return				实际读取的字节数
+		*/
+		std::string read(DWORD bytes_to_read = 512) {
+			std::string res;
+			while (!readBuffer.empty() && res.size() < bytes_to_read) {
+				std::unique_lock<std::mutex> lk(mtx);
+				res += readBuffer.front(), readBuffer.pop_front();
 			}
-		};
-	} // namespace process
+			return res;
+		}
+		/**
+		* @brief				从进程的 stdout 读取数据直到行尾
+		* @param max_bytes		要读取的最大字节数
+		* @return				实际读取的字节数
+		*/
+		std::string readLine(DWORD max_bytes = 1024) {
+			std::string res;
+			// 过滤换行符
+			while (!readBuffer.empty() && (readBuffer.front() == '\r' || readBuffer.front() == '\n')) {
+				std::unique_lock<std::mutex> lk(mtx);
+				readBuffer.pop_front();
+			}
+			while (!readBuffer.empty() && res.size() < max_bytes) {
+				std::unique_lock<std::mutex> lk(mtx);
+				if (readBuffer.front() == '\r' || readBuffer.front() == '\n') break;
+				res += readBuffer.front(), readBuffer.pop_front();
+			}
+			return res;
+		}
+		/**
+		* @brief				获取进程的返回值
+		* @retval STILL_ACTIVE	进程未退出
+		*/
+		DWORD getExitCode() {
+			DWORD code;
+			GetExitCodeProcess(process_info.hProcess, &code);
+			return code;
+		}
+		/**
+		* @brief	强制终止进程
+		*/
+		void kill(UINT exit_code = 0) {
+			signal = SG_EXIT;
+			TerminateProcess(process_info.hProcess, exit_code);
+		}
+	};
 } // namespace  mlib
